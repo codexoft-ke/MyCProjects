337cf0d4a9f79a6b9886bcf036d4eac7523e9f2f
Martin Wainaina <wainainamartin29@gmail.com>
2024-10-09 12:13:36 +0300

    New Commit

---
+++ NumberPlates.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int cars = 0;
+    for (char i = 'A'; i <= 'Z'; i++) {
+        for (char j = 'A'; j <= 'Z'; j++) {
+            for (int k = 1; k <= 999; k++) {
+                for (char final_letter = 'A'; final_letter <= 'Z'; final_letter++) {
+                    cars++;
+                }
+            }
+        }
+    }
+    printf("Kenya can register %d cars.\n", cars);
+    return 0;
+}
